c001be3e75e5d236e127734d61f217fec947f6a3
Devraj <dsdevrajsingh55555@gmail.com>
2021-07-26 18:27:40 +0530

    overflow handeling

--- first.cpp
+++ first.cpp
@@ -1,7 +1,17 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main () {
-    cout<<"Hello Guys"<<endl;
+    long long int a = 1000000000;
+    long int lng = 10000000000; //long int == int < long long int
+    long long int b = 1000000000;
+    long long int c = a*b;
+    long long int f = INT_MAX;
+    // cout<<a<<endl<<b<<endl<<c<<endl;
+    // long int d = INT_MAX;
+    // long long int e = INT_MAX;
+    // int f = INT_MAX;
+    //  cout<<d<<endl<<e<<endl<<f<<endl;
+    cout<<c<<endl<<f<<endl;
     return 0;
 }
