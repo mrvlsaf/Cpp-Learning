9f6d040cecc107f7b3d835f0280772ce8337a5b2
Devraj <dsdevrajsingh55555@gmail.com>
2021-07-27 23:54:48 +0530

    attached sample input output

--- q7.cpp
+++ q7.cpp
@@ -1,3 +1,13 @@
+// sample input: 4
+// sample output: 
+//                             4 4 4 4 4 4 4  
+//                             4 3 3 3 3 3 4   
+//                             4 3 2 2 2 3 4   
+//                             4 3 2 1 2 3 4   
+//                             4 3 2 2 2 3 4   
+//                             4 3 3 3 3 3 4   
+//                             4 4 4 4 4 4 4   
+
 #include<bits/stdc++.h>
 using namespace std;
 
