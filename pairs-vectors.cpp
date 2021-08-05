9d3bc623d500f7af7925fa40384499648742ed56
Devraj <dsdevrajsingh55555@gmail.com>
2021-08-05 23:09:00 +0530

    maps pairs

--- pairs-vectors.cpp
+++ pairs-vectors.cpp
@@ -1,20 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void printVector(vector<char> v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
     // pair<int, char> pair1;
     // pair1 = {50, 'd'};
     // cout << pair1.first << " " << pair1.second;
-    vector<char> name;
-    for (int i = 0; i < 11; i++)
+    // vector<char> name;
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     char x;
+    //     cin >> x;
+    //     name.push_back(x);
+    // }
+    // vector<char> name2 = name; //O(n) just like array using loop
+    // name2.push_back('m');
+    // printVector(name);
+    // printVector(name2);
+    // vector<int> v = {10, 20, 30, 40, 50};
+    // vector<int>::iterator it = v.begin();
+    // cout << *it;
+    vector<int> v = {1, 2, 3, 4, 5};
+    for (auto i : v)
     {
-        char x;
-        cin >> x;
-        name.push_back(x);
-    }
-    for (int i = 0; i < name.size(); i++)
-    {
-        cout << name[i] << " ";
+        cout << i << endl;
     }
 }
